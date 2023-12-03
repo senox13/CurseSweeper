b5452ca94683bac7757a1b5d719a31741209d550
senox13 <senox13@gmail.com>
2023-12-02 18:11:55 -0800

    Initial commit. Basic curses demo with movable cursor

---
+++ CurseSweeper/MinesweeperGame.cs
@@ -0,0 +1,69 @@
+﻿using Sharpie;
+using Sharpie.Abstractions;
+using System.Drawing;
+using System.Text;
+
+namespace CurseSweeper{
+    public sealed class MinesweeperGame{
+        //TODO: MinesweeperBoard instance
+        //TODO: MinesweeperBoardRenderer instance, can probably implement IDrawable
+
+        private const string cursorStr = "X";
+        private StyledText cursorRune = new(cursorStr, Style.Default);
+        private Point cursorPos = new(0, 0);
+        private Rune lastKeyPress;
+
+        private void ClampCursorPos(){
+            cursorPos = new(
+                Math.Clamp(cursorPos.X, 0, int.MaxValue), //TODO: Clamp to board size
+                Math.Clamp(cursorPos.Y, 0, int.MaxValue)
+            );
+        }
+
+        public Task Redraw(Terminal term){
+            term.Screen.Clear();
+            term.Screen.DrawBorder();
+            
+            //Draw last char
+            term.Screen.CaretLocation = new(1, 1);
+            term.Screen.WriteText(lastKeyPress.ToString());
+            //Draw cursor
+            term.Screen.CaretLocation = cursorPos;
+            term.Screen.WriteText(cursorRune);
+
+            term.Screen.Refresh();
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessEvent(ITerminal term, Event evt){
+            switch(evt){
+                case StartEvent:
+                    cursorRune = new(cursorStr, new Style(){
+                        ColorMixture = term.Colors.MixColors(StandardColor.Yellow, StandardColor.Black)
+                    });
+                    break;
+                //TODO: Should handle resize event but it never seems to be fired
+                case KeyEvent{Key: Key.Character} keyEvt:
+                    lastKeyPress = keyEvt.Char;
+                    break;
+                case KeyEvent{Key: Key.KeypadUp}:
+                    cursorPos.Offset(0, -1);
+                    ClampCursorPos();
+                    break;
+                case KeyEvent{Key: Key.KeypadDown}:
+                    cursorPos.Offset(0, 1);
+                    ClampCursorPos();
+                    break;
+                case KeyEvent{Key: Key.KeypadLeft}:
+                    cursorPos.Offset(-1, 0);
+                    ClampCursorPos();
+                    break;
+                case KeyEvent{Key: Key.KeypadRight}:
+                    cursorPos.Offset(1, 0);
+                    ClampCursorPos();
+                    break;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ CurseSweeper/Program.cs
@@ -0,0 +1,30 @@
+﻿using Sharpie;
+using Sharpie.Backend;
+
+namespace CurseSweeper{
+    internal class Program{
+        private static readonly TerminalOptions options = new(
+            ManagedWindows: true,
+            UseMouse: false,
+            CaretMode: CaretMode.Invisible
+        );
+
+        internal static void Main(string[] args){
+            Terminal terminal = null!;
+            try{
+                #pragma warning disable CA1416 // Validate platform compatibility
+                terminal = new(CursesBackend.Load(), options);
+                #pragma warning restore CA1416 // Validate platform compatibility
+                MinesweeperGame game = new();
+                terminal.Repeat(game.Redraw, 100);
+                terminal.Run(game.ProcessEvent);
+            }
+            catch(CursesInitializationException){
+                Console.WriteLine("No curses backend available");
+                Environment.Exit(-1);
+            }finally{
+                terminal?.Dispose();
+            }
+        }
+    }
+}
